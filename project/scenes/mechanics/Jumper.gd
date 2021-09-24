fb95474f48d2a77401ffda568f33bd5f0ec4a6f4
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-09-24 23:56:46 +0530

    Add Jump Mechanic scene

--- project/scenes/mechanics/Jumper.gd
+++ project/scenes/mechanics/Jumper.gd
@@ -1,19 +1,28 @@
-extends Node2D
+extends KinematicBody2D
 
-onready var rb=$KinematicBody2D
-var velocity
+onready var velocity = Vector2.ZERO
 export var gravityScale=1.0
 export var acceleration=900.0
 export var maxSpeed=400.0
 export var friction=0.25
+export var jumpForce=500.0
+export var onGround=false
 
-func _ready():
-	velocity = Vector2.ZERO
+func _process(delta):
+	if onGround and Input.is_action_just_pressed("jump"):
+			jump()
 
 func _physics_process(delta):
-	apply_gravity(delta)
 	apply_motion(delta)
-	velocity = rb.move_and_slide(velocity)
+	if is_on_floor():
+		print("ground")
+		onGround=true
+	else:
+		print("air")
+		onGround=false
+		apply_gravity(delta)
+	move_and_slide(velocity,Vector2(0.0,-1.0))
+	
 
 func apply_gravity(delta):
 	velocity.y+=9.8*gravityScale*delta*100.0
@@ -29,3 +38,7 @@ func apply_motion(delta):
 	else:
 		velocity.x += xDir*acceleration*delta
 	velocity.x=clamp(velocity.x,-maxSpeed,maxSpeed)
+
+func jump():
+	velocity.y=-jumpForce
+	onGround=false
